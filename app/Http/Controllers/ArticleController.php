f381171a076d387ec19d1b3ac0cca7bbc23d6da1
arfan <ayogaan@gmail.com>
2021-12-09 20:29:25 +0700

    add some models and seeders

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,11 +8,14 @@ class ArticleController extends Controller
 {
     public function create(){
         $categories =["ini", "itu", "anu"];
+        $types =["ini", "itu", "anu"];
         return view("insert",compact('categories'));
     }
 
-    public function store(){
-        
+    public function store(Request $request){
+        $data = $request->except("_token");
+        $data->id = 1;
+        dd($data);
     }
 
     public function edit(){
